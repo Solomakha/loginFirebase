e12913b61ec381bf5eb4f6e4bbebf032bc11a26d
Dmitry Solomaha <soldmich1995@gmail.com>
2022-07-09 19:19:22 +0300

    Добавленна проверка на заполнение полей "email" и "password" Введенные данные сохраняются в базу данных

--- loginFirebase/SignUpViewController.swift
+++ loginFirebase/SignUpViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class SignUpViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
@@ -27,5 +29,30 @@ class SignUpViewController: UIViewController {
         present(vc, animated: true)
     }
     
-
+    @IBAction func signUpPress(_ sender: Any) {
+        if emailText.text?.isEmpty == true {
+            print("No text in emailfield")
+            return
+        }
+        if passwordText.text?.isEmpty == true {
+            print("No text in passwordfield")
+            return
+        }
+        signUp()
+    }
+    
+    func signUp(){
+        Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!)
+        {
+            (authResult, error) in
+            guard let user = authResult?.user, error == nil else {
+                print("Error \(error?.localizedDescription)")
+                return
+            }
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "MainScreen")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+    }
 }
